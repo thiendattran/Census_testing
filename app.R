cc71a20587a639d75badf3952d3d708c3c172009
thiendattran <120559860+thiendattran@users.noreply.github.com>
2023-03-08 11:58:14 +0100

    Add files via upload

---
+++ app.R
@@ -0,0 +1,51 @@
+library(shiny)
+library(maps)
+library(mapproj) 
+
+# Run source code for mapping and data
+source("helpers.R")
+counties <- readRDS("data/counties.rds")
+
+# Define UI ----
+ui <- fluidPage(
+  titlePanel("censusVis"),
+  
+  sidebarLayout(
+    sidebarPanel(
+    helpText("Create demographic maps with
+           information from the 2010 US Census.",
+           
+           selectInput("var", h3("Choose a variable to display"),
+                       choices = list("Percent White",
+                                      "Percent Black",
+                                      "Percent Hispanic",
+                                      "Percent Asian")),
+           
+           sliderInput("range", "Range of interest",
+                       min = 0, max = 100, value = c(0, 100)))
+    ),
+    mainPanel(
+      plotOutput("map")
+    )
+    )
+)
+# Define server logic ----
+server <- function(input, output){
+  output$map <- renderPlot({
+    args <- switch(input$var,
+                   "Percent White" = list(counties$white, "darkgreen","% white"),
+                   "Percent Black" = list(counties$black, "black","% black"),
+                   "Percent Hispanic" = list(counties$hispanic, "darkorange","% hispanic"),
+                   "Percent Asian" = list(counties$asian, "darkviolet","% asian"),
+                   )
+    
+    args$min <- input$range[1]
+    args$max <- input$range[2]
+    
+    do.call(percent_map,args)
+  })
+}
+
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
